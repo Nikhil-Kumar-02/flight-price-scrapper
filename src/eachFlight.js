ddf3544b03acf36be3c741e6cf8b642c44f64c3d
Nikhil Kumar <nikhil.pmsnick@gmail.com>
2023-11-13 21:12:53 +0530

    created the card for the flight search result

--- src/eachFlight.js
+++ src/eachFlight.js
@@ -1,9 +1,40 @@
 import React from "react"
+import './eachFlight.css'
+import plane from './img/plane.png'
 
 const EachFlight = (props) => {
   return (
     <div className="eachFlightContainer">
-        <div></div>
+        <div className="planeLogo">
+            <p>indigo + scoot</p>
+        </div>
+        <div className="jouneyFrom">
+            <div>17h : 30m</div>
+            <div>BLR</div>
+        </div>
+        <div className="cardMid">
+            <div className="journeyTime">
+                11h : 45m
+            </div>
+            <div className="hrline"></div>
+            <div className="flightType">
+                Direct
+            </div>
+        </div>
+        <div className="planeImage">
+            <img src={plane} height={15}></img>
+        </div>
+        <div className="journeyTo">
+            <div>10h : 45m</div>
+            <div>SYD</div>
+        </div>
+        <div className="verticalLine"></div>
+        <div className="rightside">
+            <div className="journeyPrice">₹58,323</div>
+            <div className="btn">
+                <button>Select →</button>
+            </div>
+        </div>
     </div>
   )
 };
--- src/navbar.js
+++ src/navbar.js
@@ -1,18 +1,25 @@
-import React, { useState } from "react"
+import React from "react"
 import flight_icon from './img/flight_icon.png'
 import user from './img/user.png'
 import './navbar.css'
 import JourneyDetails from "./journeyDetails";
+import EachFlight from "./eachFlight";
+
 
 const Navbar = (props) => {
   return (
     <div className="navbar_container">
+      <div className="navbar_searchbar">
         <div className="login_icon_bar">
-            <img src={flight_icon} alt='logo' height={50}></img>
-            <img src={user} alt='logo' height={30}></img>
+          <img src={flight_icon} alt='logo' height={50}></img>
+          <img src={user} alt='logo' height={30}></img>
         </div>
         <h2>Millions of cheap flights. One simple search.</h2>
         <JourneyDetails></JourneyDetails>
+      </div>
+      <div className="flightResults">
+        <EachFlight></EachFlight>
+      </div>
     </div>
   )
 };
